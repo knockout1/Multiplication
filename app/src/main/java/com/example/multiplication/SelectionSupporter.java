b7dd995b9520a46b7cdc2d183da38a0311cbc1d1
Karol Olek <karol.olek@sabre.com>
2019-08-16 15:41:36 +0200

    code refactore

--- app/src/main/java/com/example/multiplication/SelectionSupporter.java
+++ app/src/main/java/com/example/multiplication/SelectionSupporter.java
@@ -1,37 +1,32 @@
 package com.example.multiplication;
 
-import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Switch;
 
 import java.util.ArrayList;
 
-public class SelectionSupporter {
+class SelectionSupporter {
 
     private LinearLayout multiplicationSetupLayout;
 
-    SelectionSupporter(LinearLayout multiplicationSetupLayout){
+    SelectionSupporter(LinearLayout multiplicationSetupLayout) {
         this.multiplicationSetupLayout = multiplicationSetupLayout;
     }
 
-    boolean checkIfAnythingSelected(){
+    boolean checkIfAnythingSelected() {
         return getCheckedElements().size() > 0;
     }
 
     ArrayList<Integer> getCheckedElements() {
         ArrayList<Integer> checkedElements = new ArrayList<>();
-        int count = multiplicationSetupLayout.getChildCount();
-        Switch switchElement;
-        for (int i = 0; i < count; i++) {
-            View view = multiplicationSetupLayout.getChildAt(i);
-            if (view instanceof Switch) {
-                switchElement = (Switch) multiplicationSetupLayout.getChildAt(i);
+        for (int i = 0; i < multiplicationSetupLayout.getChildCount(); i++) {
+            if (multiplicationSetupLayout.getChildAt(i) instanceof Switch) {
+                Switch switchElement = (Switch) multiplicationSetupLayout.getChildAt(i);
                 if (switchElement.isChecked()) {
-                    checkedElements.add(i+1);
+                    checkedElements.add(i + 1);
                 }
             }
         }
         return checkedElements;
     }
-
 }
